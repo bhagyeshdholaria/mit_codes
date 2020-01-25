ab7cd69e60f0afddc79d70c5a13349f6df4cdc3c
bhagyeshdholaria <bhagyeshdholaria@gmail.com>
2020-01-25 18:21:13 +0530

    fa

--- HPC/quicksort.c
+++ HPC/quicksort.c
@@ -2,14 +2,15 @@
 #include<stdlib.h>
 #include<omp.h>
 #include<time.h>
-#define n 30000
-int arr1[n],arr2[n];
+//#define n 20000
+int n;
+
 /* This function takes first element as pivot, places
    the pivot element at its correct position in sorted
     array, and places all smaller (smaller than pivot)
    to left of pivot and all greater elements to right
    of pivot *///
-int partition(int arr[], int low, int high){
+int partition(double arr[], int low, int high){
 	int i, j, temp, key;
 	key = arr[low];
 	i= low + 1;
@@ -37,7 +38,7 @@ int partition(int arr[], int low, int high){
  arr[] --> Array to be sorted,
   low  --> Starting index,
   high  --> Ending index */
-void quicksort(int arr[], int low, int high)
+void quicksort(double arr[], int low, int high)
 {
 	int j;
 
@@ -50,7 +51,8 @@ void quicksort(int arr[], int low, int high)
 	}
 }
 
-void parallel_quicksort(int arr[], int low, int high)
+/*
+void parallel_quicksort(double arr[], int low, int high)
 {
 	int j;
 
@@ -72,9 +74,14 @@ void parallel_quicksort(int arr[], int low, int high)
 
 	}
 }
+*/
 
 int main()
 {
+	
+	printf("Enter number of elements:");
+	scanf("%d",&n);
+	double arr1[n],arr2[n];
 	for(int i=0;i<n;i++){
 		
 		arr2[i]= arr1[i]= n-i;
@@ -82,28 +89,24 @@ int main()
 	int j;
 	double start_s,stop_s;
 
-// for(int j =0;j<n;j++){
-// 		printf("%d ", arr1[j]);
-// 	}
+//serial
 
 	start_s= omp_get_wtime();//start time
 	quicksort(arr1,0,n-1);
 	stop_s= omp_get_wtime();//end Time
 	printf("Time taken by serial: %.4fs\n\n", (double)(stop_s - start_s));
 
-// for(int j =0;j<n;j++){
-// 		printf("%d ", arr2[j]);
-// 	}
 
-	start_s= omp_get_wtime();//start time
-	parallel_quicksort(arr2,0,n-1);
-	stop_s= omp_get_wtime();//end Time
-	printf("\nTime taken by parallel: %.4fs\n", (double)(stop_s - start_s));
+//parallel
+	// start_s= omp_get_wtime();//start time
+	// parallel_quicksort(arr2,0,n-1);
+	// stop_s= omp_get_wtime();//end Time
+	// printf("\nTime taken by parallel: %.4fs\n", (double)(stop_s - start_s));
 
 	
 
 
-/*
+	start_s= omp_get_wtime();//start time
 	j = partition(arr1,0,n-1);// returns the pivot element
 		#pragma omp parallel sections
 		{
@@ -118,17 +121,14 @@ int main()
 		}
 	stop_s= omp_get_wtime();//end Time
 	printf("Time taken by parallel: %.4fs\n", (double)(stop_s - start_s));
-	
-	// for(int j =0;j<n;j++){
-	// 	printf("%d ", arr[j]);
-	// }
 
+/*
 	start_s= omp_get_wtime();//start time
 	j = partition(arr2,0,n-1);// returns the pivot element
 	quicksort(arr2,0, j - 1);//Thread 1
 	quicksort(arr2, j + 1, n-1);//Thread 2
 	stop_s= omp_get_wtime();//end Time
 	printf("Time taken by serial: %.4fs\n", (double)(stop_s - start_s));
-*/
 
+*/
 }
